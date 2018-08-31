c57f94460e2d222a16e12096218bc0669dff602b
tanner0101 <me@tanner.xyz>
2018-08-31 14:45:22 -0400

    disable logs

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -34,7 +34,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     /// Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
     databases.add(database: sqlite, as: .sqlite)
-    databases.enableLogging(on: .sqlite)
+    // databases.enableLogging(on: .sqlite)
     services.register(databases)
 
     /// Configure migrations
